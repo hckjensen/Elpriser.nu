ab09357ebfa91b516f03a7d13cde5baecb1990b7
hckjensen <hckjensen96@gmail.com>
2023-11-02 20:46:14 +0100

    ugigugi

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,65 +16,10 @@ if (currentDay < 10){
 
 // Format the date as a string (MM/DD/YYYY or DD/MM/YYYY)
 const formattedDate = `${currentDay}-${currentMonth}-${currentYear}`;
-document.getElementById('selected-date').innerHTML = `${formattedDate}`
+
 
 // Display the current date
 console.log("Current Date:", formattedDate);
-
-
-//OVERSIGT
-
-const url = `https://www.elprisenligenu.dk/api/v1/prices/${currentYear}/${currentMonth}-${currentDay}_DK1.json`
-
-
-const getData = () => {
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            // Find højeste og laveste pris
-            const prices = data.map(item => item.DKK_per_kWh);
-            const minPrice = Math.min(...prices);
-            const maxPrice = Math.max(...prices);
-
-            // Function to generate a color based on the value
-            function getColor(value) {
-                // Calculate a value between 0 and 1 based on the item's position between min and max
-                const normalizedValue = (value - minPrice) / (maxPrice - minPrice);
-                // Use a gradient from red to green based on the normalized value
-                const hue = (1 - normalizedValue) * 120;
-                // Convert HSL to RGB
-                const rgbValue = `hsl(${hue}, 100%, 50%)`;
-                return rgbValue;
-            }
-
-
-
-
-            // Opdater HTML-elementerne med priserne
-            for (let i = 0; i < data.length; i++) {
-                const prisElement = document.getElementById(`pris${i}`);
-                const pris = data[i].DKK_per_kWh;
-                prisElement.innerHTML = pris.toFixed(3) + ' ' + 'kr';
-                prisElement.style.color = getColor(pris)
-                 
-            }
-
-            // Lav og Høj pris
-            const minimumPrice = document.getElementById('minPrice')
-            const maximumPrice = document.getElementById('maxPrice')
-            minimumPrice.innerHTML = `${minPrice.toFixed(3)} kr`
-            maximumPrice.innerHTML = `${maxPrice.toFixed(3)} kr`
-
-
-        })
-}
-
-getData()
-
-
-
-
 
 
 // NAV-BAR select
@@ -107,9 +52,63 @@ navItem.forEach(item => {
     })
 });
 
+
+//OVERSIGT
+
+const url = `https://www.elprisenligenu.dk/api/v1/prices/${currentYear}/${currentMonth}-${currentDay}_DK1.json`
+
+
+const getData = () => {
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            // Find højeste og laveste pris
+            const prices = data.map(item => item.DKK_per_kWh);
+            const minPrice = Math.min(...prices);
+            const maxPrice = Math.max(...prices);
+
+            // Function to generate a color based on the value
+            function getColor(value) {
+                // Calculate a value between 0 and 1 based on the item's position between min and max
+                const normalizedValue = (value - minPrice) / (maxPrice - minPrice);
+                // Use a gradient from red to green based on the normalized value
+                const hue = (1 - normalizedValue) * 120;
+                // Convert HSL to RGB
+                const rgbValue = `hsl(${hue}, 100%, 50%)`;
+                return rgbValue;
+            }
+
+            // Opdater HTML-elementerne med priserne
+            for (let i = 0; i < data.length; i++) {
+                const prisElement = document.getElementById(`pris${i}`);
+                const nyPrisElement = document.getElementById(`nypris${i}`);
+                const pris = data[i].DKK_per_kWh;
+                prisElement.innerHTML = pris.toFixed(3) + ' ' + 'kr';
+                nyPrisElement.innerHTML = pris.toFixed(3) + ' ' + 'kr';
+                prisElement.style.color = getColor(pris)
+                nyPrisElement.style.color = getColor(pris)
+                 
+            }
+            // Lav og Høj pris
+            const minimumPrice = document.getElementById('minPrice')
+            const maximumPrice = document.getElementById('maxPrice')
+            minimumPrice.innerHTML = `${minPrice.toFixed(3)} kr`
+            maximumPrice.innerHTML = `${maxPrice.toFixed(3)} kr`
+
+
+        })
+}
+
+getData()
+
+
+
 // Pick Date
 
 const pickDate = document.getElementById('datepicker')
+
+document.getElementById('selected-date').innerHTML = `${formattedDate}`
 
 pickDate.addEventListener('change', function(){
     const newDate = new Date(this.value);
@@ -117,14 +116,57 @@ pickDate.addEventListener('change', function(){
 
     const newYear = newDate.getFullYear();
     const newMonth = newDate.getMonth() + 1; // Adding 1 because getMonth() returns values from 0 to 11.
-    const newDay = newDate.getDate();
+    let newDay = newDate.getDate();
+
+    if (newDay < 10){
+        newDay = '0' + newDay
+    } else {
+        newDay = newDay
+    }
 
     selectedDate.innerHTML = `${newDay}-${newMonth}-${newYear}`
 
 
-    console.log('Year: ' + newYear);
-    console.log('Month: ' + newMonth);
-    console.log('Date: ' + newDay);
+    // Generate prices for selected day
+
+    const newUrl = `https://www.elprisenligenu.dk/api/v1/prices/${newYear}/${newMonth}-${newDay}_DK1.json`
+
+    fetch(newUrl)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            const prices = data.map(item => item.DKK_per_kWh);
+            const minPrice = Math.min(...prices);
+            const maxPrice = Math.max(...prices);
+
+            // Function to generate a color based on the value
+            function getColor(value) {
+                // Calculate a value between 0 and 1 based on the item's position between min and max
+                const normalizedValue = (value - minPrice) / (maxPrice - minPrice);
+                // Use a gradient from red to green based on the normalized value
+                const hue = (1 - normalizedValue) * 120;
+                // Convert HSL to RGB
+                const rgbValue = `hsl(${hue}, 100%, 50%)`;
+                return rgbValue;
+            }
+
+            // Opdater HTML-elementerne med priserne
+            for (let i = 0; i < data.length; i++) {
+                const prisElement = document.getElementById(`nypris${i}`);
+                const pris = data[i].DKK_per_kWh;
+                prisElement.innerHTML = pris.toFixed(3) + ' ' + 'kr';
+                prisElement.style.color = getColor(pris)
+                 
+            }
+        })
+        .catch(()=>{
+            console.log("no data")
+            for (let i = 0; i < 24; i++) {
+                const prisElement = document.getElementById(`nypris${i}`)
+                prisElement.innerHTML = "No Data"
+                prisElement.style.color = "red"
+            }
+        })
 })
 
 
